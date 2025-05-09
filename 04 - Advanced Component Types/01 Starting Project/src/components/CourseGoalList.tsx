e5e54f193d6bab96d44151417f9df2452010e200
Xnaizer <seykig028@gmail.com>
2025-05-09 10:49:45 +0700

    update

--- 04 - Advanced Component Types/01 Starting Project/src/components/CourseGoalList.tsx
+++ 04 - Advanced Component Types/01 Starting Project/src/components/CourseGoalList.tsx
@@ -1,6 +1,7 @@
 import CourseGoal from './CourseGoal.tsx';
 import { type CourseGoal as CGoal } from '../App.tsx';
 import InfoBox from './InfoBox.tsx';
+import { type ReactNode } from 'react';
 
 type CourseGoalListProps = {
   goals: CGoal[];
@@ -11,22 +12,41 @@ export default function CourseGoalList({
   goals,
   onDeleteGoal,
 }: CourseGoalListProps) {
+
   if(goals.length === 0) {
     return <InfoBox mode='hint'>
       Add Your Activity Course!
     </InfoBox>
   }
 
+  let warningBox : ReactNode;
+
+  if (goals.length >= 6 ) {
+    warningBox = (<InfoBox mode='warning' severity='high'>
+     TOOO MUCHH!
+    </InfoBox>)
+  } else if (goals.length >= 4) {
+    warningBox = (<InfoBox mode='warning' severity='medium'>
+      You need to do your task
+    </InfoBox>)
+  } else {
+    warningBox = (<InfoBox mode='warning' severity='low'>
+      Get Task Going!
+    </InfoBox>)
+  }
 
   return (
-    <ul>
-      {goals.map((goal) => (
-        <li key={goal.id}>
-          <CourseGoal id={goal.id} title={goal.title} onDelete={onDeleteGoal}>
-            <p>{goal.description}</p>
-          </CourseGoal>
-        </li>
-      ))}
-    </ul>
+    <>
+      {warningBox}
+      <ul>
+        {goals.map((goal) => (
+          <li key={goal.id}>
+            <CourseGoal id={goal.id} title={goal.title} onDelete={onDeleteGoal}>
+              <p>{goal.description}</p>
+            </CourseGoal>
+          </li>
+        ))}
+      </ul>
+    </>
   );
 }
--- 04 - Advanced Component Types/01 Starting Project/src/components/InfoBox.tsx
+++ 04 - Advanced Component Types/01 Starting Project/src/components/InfoBox.tsx
@@ -1,12 +1,32 @@
 
 import {type ReactNode } from "react"
 
-type InfoBoxProps = {
-    mode : 'hint' | 'warning',
+// type InfoBoxProps = {
+//     mode : 'hint' | 'warning';
+//     severity?: 'low' | 'medium' | 'high';
+//     // severity: 'low' | 'medium' | 'high' | undefined; // jika ga pake ?
+//     children: ReactNode;
+// }
+
+type WarningBoxProps = {
+    mode : 'warning';
+    severity: 'low' | 'medium' | 'high';
     children: ReactNode;
 }
 
-export default function InfoBox({mode, children} : InfoBoxProps) {
+type HintBoxProps = {
+    mode : 'hint';
+    children: ReactNode;
+}
+
+type InfoBoxProps = HintBoxProps | WarningBoxProps;
+
+export default function InfoBox(
+    // {mode,severity, children} : InfoBoxProps
+    props: InfoBoxProps
+
+) {
+    const {children, mode} = props;
     if (mode === 'hint') {
         return (
             <aside className="infobox infobox-hint">
@@ -15,9 +35,9 @@ export default function InfoBox({mode, children} : InfoBoxProps) {
          )
     }
 
-
+    const { severity } = props; // ini bisa karna react ngerti klo ini bukan termasuk mode hint jadi karna di taruh dibawah fungsi diatas, jadi bisa digunakan, karna kalo ditaruh diatas severity akan digunakan di hint padahal engga.
     return (
-    <aside className="infobox infobox-warning warning--high">
+    <aside className={`infobox infobox-warning warning--${severity}`}>
         {/* {mode == 'warning' ? <h2>Warning</h2> : null}   */}
         <h2>Warning</h2> 
         <p>{children}</p>
